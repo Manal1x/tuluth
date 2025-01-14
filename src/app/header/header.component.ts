230449034154f6cf389d850516e82a480685a154
Manal1x <Manal1.34n@gmail.com>
2025-01-14 14:14:36 +0300

    Header done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: false,
-  
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuValue:boolean = false;
+  menuIcon:String = 'bi bi-list';
+
+
+  openMenu(){
+    this.menuValue =! this.menuValue;
+    this.menuIcon = this.menuValue? 'bi bi-x': 'bi bi-list';
+  }
+  
+  closeMenu(){
+    this.menuValue = false;
+    this.menuIcon = 'bi bi-list';
+  }
 
 }
